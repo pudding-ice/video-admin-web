a591f4a934d51694e7052d09f323f2d11563d28a
guochun <1076294651@qq.com>
2022-08-05 13:36:16 +0800

    20. 后台管理-修复面包屑跳转404

--- vue-admin-template/src/components/Breadcrumb/index.vue
+++ vue-admin-template/src/components/Breadcrumb/index.vue
@@ -29,34 +29,28 @@ export default {
   methods: {
     getBreadcrumb() {
       // only show routes with meta.title
-      let matched = this.$route.matched.filter(item => item.meta && item.meta.title)
-      const first = matched[0]
-
-      if (!this.isDashboard(first)) {
-        matched = [{ path: '/dashboard', meta: { title: 'Dashboard' }}].concat(matched)
-      }
-
+      const matched = this.$route.matched.filter(item => item.meta && item.meta.title)
       this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
     },
-    isDashboard(route) {
-      const name = route && route.name
-      if (!name) {
-        return false
-      }
-      return name.trim().toLocaleLowerCase() === 'Dashboard'.toLocaleLowerCase()
-    },
+
     pathCompile(path) {
-      // To solve this problem https://github.com/PanJiaChen/vue-element-admin/issues/561
-      const { params } = this.$route
-      var toPath = pathToRegexp.compile(path)
+      let toPath
+      const {params} = this.$route
+      if (path === '/authority') {
+        toPath = pathToRegexp.compile(path + '/user/list')
+      } else {
+        toPath = pathToRegexp.compile(path + '/list')
+      }
       return toPath(params)
     },
     handleLink(item) {
+      console.log(item)
       const { redirect, path } = item
-      if (redirect) {
+      if (redirect !== 'noredirect') {
         this.$router.push(redirect)
         return
       }
+      console.log(this.pathCompile(path))
       this.$router.push(this.pathCompile(path))
     }
   }
